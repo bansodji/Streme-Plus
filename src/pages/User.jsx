210c837ad605d90114e2ac4dbfab16d229554673
Ashish Bansod <86002886+bansodji@users.noreply.github.com>
2023-11-26 16:20:03 +0530

    user page created

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,8 +1,44 @@
-import React from 'react';
+import React, { useLayoutEffect } from 'react';
+import styled from 'styled-components';
+import { GradientButton } from '../styles/Buttons';
+import { Link } from 'react-router-dom';
 
-const User = () => {
+const Wrapper = styled.section`
+  width: 100%;
+  height: 90vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  .brand{
+    img{
+      width: 200px;
+    }
+  }
+`;
+
+const User = ({ setProgress }) => {
+  useLayoutEffect(() => {
+    setProgress(10);
+    setTimeout(() => {
+      setProgress(100);
+    }, 200)
+
+  }, []);
+
   return (
-    <div>User</div>
+    <Wrapper>
+      <center className='container'>
+        <h1>Login to
+          &nbsp;
+          <Link to="/" className='brand'>
+            <img src="/images/logo.png" alt="Streme+" />
+          </Link>
+        </h1>
+        <p className='font-400'>Start watching from where you left off, personalise for kids and more</p>
+        <GradientButton>Login</GradientButton>
+      </center>
+    </Wrapper>
   )
 }
 
